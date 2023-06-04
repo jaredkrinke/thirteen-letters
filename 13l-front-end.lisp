1b91f36c1cbdbefde03d097e17d1eac5019925d8
Jared Krinke <jared.krinke@gmail.com>
2023-06-04 01:42:53 -0700

    Playable game

--- 13l-front-end.lisp
+++ 13l-front-end.lisp
@@ -52,11 +52,8 @@
 	   (guess-input (get-id "guess"))
 	   (winner-span (get-id "winner"))
 	   (winning-word-span (get-id "winning-word")))
-       (defun hide (element)
-	 ((ps:chain element class-list add) "hidden"))
-       
-       (defun show (element)
-	 ((ps:chain element class-list remove) "hidden"))
+       (defun hide (element) ((ps:chain element class-list add) "hidden"))
+       (defun show (element)((ps:chain element class-list remove) "hidden"))
        
        (defun update-letters (word)
 	 (dotimes (i (ps:chain word length))
@@ -65,16 +62,20 @@
        
        (defun round-started (message)
 	 (setf scrambled (ps:chain message scrambled))
+	 (show guess-input)
 	 (hide result-div)
+	 (setf (ps:chain guess-input value) "")
+	 ((ps:chain guess-input focus))
 	 (update-letters scrambled))
        
        (defun round-ended (message)
 	 (show result-div)
+	 (hide guess-input)
 	 (let* ((entries (ps:chain message leaderboard))
 		(winning-entry (and entries (aref entries 0))))
 	   (update-letters (ps:chain message solution))
 	   (setf (ps:chain winner-span inner-text) (if winning-entry (ps:chain winning-entry name) "(no one)"))
-	   (setf (ps:chain winning-word-span inner-text) (if winning-entry (ps:chain winning-entry word) "(none)"))))
+	   (setf (ps:chain winning-word-span inner-text) (if winning-entry (ps:chain winning-entry word) "---"))))
        
        (defun update-leaderboard (message)
 	 (let ((entries (ps:chain message leaderboard)))
@@ -87,8 +88,17 @@
 		   (append-text col-name (ps:chain entry name))
 		   ((ps:chain row append-child) col-name)
 		   (append-text col-word (ps:chain entry word))
+		   ((ps:chain col-word class-list add) "right")
+		   ((ps:chain col-word class-list add) "caps")
 		   ((ps:chain row append-child) col-word)
-		   ((ps:chain tbody append-child) row))))))
+		   ((ps:chain tbody append-child) row)))
+	       (let ((row ((ps:chain document create-element) "tr"))
+		     (col ((ps:chain document create-element) "td")))
+		 ((ps:chain col set-attribute) "colspan" 2)
+		 ((ps:chain col class-list add) "center")
+		 (append-text col "(empty)")
+		 ((ps:chain row append-child) col)
+		 ((ps:chain tbody append-child) row)))))
        
        (defun handle-error ()
 	 (error "WebSocket error!"))
@@ -120,6 +130,8 @@
        (defun handle-start ()
 	 (watch
 	  (hide intro-div)
+	  (show main-div)
+	  (show top-div)
 	  (if (not socket)
 	      (progn (setf socket (ps:new (-web-socket *web-socket-uri*)))
 		     (setf (ps:chain socket onopen) send-rename)
@@ -147,9 +159,42 @@
 ;;; CSS
 (defparameter *css*
   (cl-css:css
-   '(
-     (.hidden :display "none")
-     )))
+   '(("html, body" :margin 0)
+     ("body, button, input" :font-family "'Cascadia Mono', 'Consolas', monospace")
+     ("table" :width "100%")
+     ("input" :font-size "100%")
+     
+     (".left" :text-align "left")
+     (".center" :text-align "center")
+     (".right" :text-align "right")
+     (".caps" :text-transform "uppercase")
+     
+     (".main" :display "flex"
+	      :flex-direction "column"
+	      :align-items "center")
+     (".top" :width "100%"
+	     :display "flex"
+	     :flex-direction "column"
+	     :margin-top "1em")
+     ("#root" :display "flex"
+	      :flex-direction "column"
+	      :align-items "center"
+	      :max-width "40em"
+	      :margin "auto")
+     ("#guess, #result" :font-size "150%"
+			:margin-top "1em")
+     ("#start" :margin "1em"
+	       :padding "0.25em 1em"
+	       :font-size "150%"
+	       :font-weight "bold")
+     ("#letters" :margin "1em")
+     
+     ("#letters > span" :margin "0.5em"
+			:padding "0.25em"
+			:border "0.1em solid"
+			:text-transform "uppercase")
+     (.invisible :display "hidden")
+     (.hidden :display "none"))))
 
 ;;; HTML
 (defparameter *intro* (sp:with-html-string ("**Thirteen Letters** is a game where players compete to find the longest word that can be constructed from the given set of letters.
@@ -169,28 +214,29 @@ It's also a game that needs better documentation!")))
 (defun make-index-html ()
   "Outputs root HTML page"
   (page (:title "(thirteen-letters)")
-    (:h1 "(thirteen-letters)")
-    (:div :id "intro"
-	  (:h2 "Welcome!")
-	  (:raw *intro*)
-	  "Name: " (:input :id "name" :type "text")
-	  (:br)
-	  (:button :id "start" "Start"))
-    (:div :id "main"
-	  (:div :id "letters"
-		(dotimes (n 13)
-		  (:span :id (format nil "l~d" n) "?")))
-	  (:input :id "guess" :type "text"))
-    (:div :id "top"
-	  (:h3 "Leaderboard")
-	  (:table (:thead (:tr (:th "Name") (:th "Word")))
-		  (:tbody :id "tbody")))
-    (:div :id "result" :class "hidden"
-	  (:h2 "End of round")
-	  (:p "Winner: " (:span :id "winner"))
-	  (:p "Word: " (:span :id "winning-word")))
-    (:div :id "debug"
-	  (:h2 "Debug log"))
+    (:div :id "root"
+	  (:h1 "(thirteen-letters)")
+	  (:div :id "intro"
+		(:h2 "Welcome!")
+		(:raw *intro*)
+		(:p "Name: " (:input :id "name" :type "text"))
+		(:p :class "center"
+		    (:button :id "start" "Start")))
+	  (:div :id "main" :class "hidden main"
+		(:div :id "letters"
+		      (dotimes (n 13)
+			(:span :id (format nil "l~d" n) "?")))
+		(:input :id "guess" :type "text"))
+	  (:div :id "result" :class "hidden"
+		"Winner: "
+		(:span :id "winner")
+		" ("
+		(:span :id "winning-word" :class "caps")
+		")")
+	  (:div :id "top" :class "top hidden"
+		(:h3 :class "center" "Leaderboard")
+		(:table (:tbody :id "tbody")))
+	  (:div :id "debug"))
     (:script (:raw *script*))))
 
 
